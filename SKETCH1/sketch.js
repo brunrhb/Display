44d60ce3349152fe3f4f2bc89b189a24f1333c2b
Brunr <brunr.hb@gmail.com>
2023-07-12 16:41:41 +0200

    Oui mais Non, SKETCH 1

--- SKETCH1/sketch.js
+++ SKETCH1/sketch.js
@@ -1,25 +1,14 @@
-let Phrases = ['Une','Dos','Tres','VivalAlgerie'];
-let i = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  textSize(windowHeight / 10);
-  textAlign(CENTER, CENTER);
 }
 
 function draw() {
   background(220);
-  text(Phrases[i], windowWidth/2, windowHeight/2);
-}
 
-function mouseClicked() {
-  i = (i + 1) % Phrases.length;
-}
+  
+textSize(randomGaussian(3, 10))  
+text ('Non', mouseX+ randomGaussian(3, 10), mouseY+randomGaussian(3,10));
 
-window.onload = function() {
-    // Vérifiez si l'utilisateur vient de la page d'accueil
-    if (document.referrer.includes('index.html')) {
-        // Si oui, affichez le lien de retour
-        document.getElementById('back-link').style.display = 'block';
-    }
 }
