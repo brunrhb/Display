cb3b279bd26691c1edbf1cbe076205353271f30a
brunrhb <77405180+brunrhb@users.noreply.github.com>
2023-07-14 20:42:29 +0200

    formattage nude

--- script.js
+++ script.js
@@ -23,38 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
         vidHeadContainer.style.transform = `scale(${scale})`;
     });
 
-    // Ajoute un gestionnaire d'événement pour ajuster la taille du footer lors du scroll
-    const footer = document.getElementById('footer');
-    const footerContainer = document.getElementById('footerContainer');
-    const footerImg = document.getElementById('footerImg');
-    window.addEventListener('scroll', () => {
-        const scrollPosition = (window.pageYOffset - window.innerHeight) / (document.body.scrollHeight - 2 * window.innerHeight);
-        if (scrollPosition > 0) {
-            const opacity = scrollPosition * 0.3; // Apparition progressive de 30% de noir
-            footerContainer.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
-            const translateY = -150 * scrollPosition; // L'image monte de 150px lors du défilement
-            footerImg.style.transform = `translateY(${translateY}px)`;
-        }
-    });
 
-    // Fetch data from SKETCH1/ID.js and SKETCH2/ID.js and insert into the HTML elements
-    fetch('SKETCH1/ID.js')
-    .then(response => response.text())
-    .then(data => {
-        eval(data);
-        const description1 = document.getElementById('description1');
-        if (description1) {
-            description1.innerHTML += `<br>${txtNomSKETCH1}<br>${txtDescriptionSKETCH1}`;
-        }
-    });
 
-    fetch('SKETCH2/ID.js')
-    .then(response => response.text())
-    .then(data => {
-        eval(data);
-        const description2 = document.getElementById('description2');
-        if (description2) {
-            description2.innerHTML += `<br>${txtNomSKETCH2}<br>${txtDescriptionSKETCH2}`;
-        }
-    });
-});
+
+    
