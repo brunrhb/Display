2baf36dd844f971176774765be05a7b8d4b14e84
brunrhb <77405180+brunrhb@users.noreply.github.com>
2023-07-14 21:11:41 +0200

    Anime header ok

--- script.js
+++ script.js
@@ -10,8 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
         const descriptionElement = item.querySelector('.description');
         const creationDate = new Date().toLocaleDateString('fr-FR');
         descriptionElement.id = `description${index + 1}`;
-        descriptionElement.innerHTML = `<span id="txtNumero">${index + 1}</span><br><span id="txtDate">${creationDate}</span><br>${descriptionElement.innerHTML}`;
+        descriptionElement.innerHTML = `<span id=\"txtNumero\">${index + 1}</span><br><span id=\"txtDate\">${creationDate}</span><br>${descriptionElement.innerHTML}`;
     });
+
+    // Ajoute un gestionnaire d'événement pour faire défiler les frames de la vidéo lors du scroll
+    const vidHead = document.getElementById('vidHead');
+    const vidHeadContainer = document.getElementById('vidHeadContainer');
+    if (vidHead && vidHeadContainer) {
+        window.addEventListener('scroll', () => {
+            const scrollPosition = window.pageYOffset / window.innerHeight;
+            vidHead.currentTime = scrollPosition * vidHead.duration;
+            const scale = 1.2 - scrollPosition * 0.4; // La taille de la vidéo passe de 120% à 80% lors du défilement
+            vidHeadContainer.style.transform = `scale(${scale})`;
+        });
+    }
 
     // Fetch data from SKETCH1/ID.js and SKETCH2/ID.js and insert into the HTML elements
     fetch('SKETCH1/ID.js')
@@ -20,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
         eval(data);
         const description1 = document.getElementById('description1');
         if (description1) {
-            description1.innerHTML += `<br><span id="txtNom">${txtNomSKETCH1}</span><br><span id="txtDescription">${txtDescriptionSKETCH1}</span>`;
+            description1.innerHTML += `<br><span id=\"txtNom\">${txtNomSKETCH1}</span><br><span id=\"txtDescription\">${txtDescriptionSKETCH1}</span>`;
         }
     });
 
@@ -30,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function() {
         eval(data);
         const description2 = document.getElementById('description2');
         if (description2) {
-            description2.innerHTML += `<br><span id="txtNom">${txtNomSKETCH2}</span><br><span id="txtDescription">${txtDescriptionSKETCH2}</span>`;
+            description2.innerHTML += `<br><span id=\"txtNom\">${txtNomSKETCH2}</span><br><span id=\"txtDescription\">${txtDescriptionSKETCH2}</span>`;
         }
     });
 });
