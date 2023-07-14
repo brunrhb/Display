3c3fcf4acc948b9c967519918f89a47686506366
brunrhb <77405180+brunrhb@users.noreply.github.com>
2023-07-14 21:32:00 +0200

    SKETCH1 Fixed

--- SKETCH1/sketch.js
+++ SKETCH1/sketch.js
@@ -1,10 +1,11 @@
+let txtNom = 'Non'; // Définition de la variable txtNom
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); // Création du canvas
 }
 
 function draw() {
-  background(220);
-  textSize(randomGaussian(3, 10))  
-  text (txtNom, mouseX+ randomGaussian(3, 10), mouseY+randomGaussian(3,10));
+  background(220); // Définition de la couleur d'arrière-plan
+  textSize(randomGaussian(3, 10)); // Définition de la taille du texte
+  text(txtNom, mouseX+ randomGaussian(3, 10), mouseY+randomGaussian(3,10)); // Affichage du texte
 }
